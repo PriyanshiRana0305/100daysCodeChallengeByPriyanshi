f00ebafbd908a54dfff398d136143f3f9daeea8c
PriyanshiRana0305 <priyanshirana0305@gmail.com>
2025-09-26 12:16:52 +0530

    Add files via upload

---
+++ day4Q7.c
@@ -0,0 +1,8 @@
+//Q7: Write a program to swap two numbers without using a third variable.
+#include<stdio.h>
+int main() {
+    int a;
+    printf("Enter the number: ");
+    scanf("%d",a);
+        
+}
