560b6684ebe90b31d32328b17eff899429f46d82
PriyanshiRana0305 <priyanshirana0305@gmail.com>
2025-10-01 20:05:33 +0530

    Add files via upload

---
+++ day11Q22.c
@@ -0,0 +1,25 @@
+//Q22: Write a program to find profit or loss percentage given cost price and selling price.
+
+#include <stdio.h>
+
+int main() {
+    double costPrice, sellingPrice;
+    printf("Enter Cost Price and Selling Price: ");
+    scanf("%lf %lf", &costPrice, &sellingPrice);
+
+    if (sellingPrice > costPrice) {
+        double profit = sellingPrice - costPrice;
+        double profitPercent = (profit / costPrice) * 100;
+        printf("Profit %.0lf%%\n", profitPercent);
+    } 
+    else if (costPrice > sellingPrice) {
+        double loss = costPrice - sellingPrice;
+        double lossPercent = (loss / costPrice) * 100;
+        printf("Loss %.0lf%%\n", lossPercent);
+    } 
+    else {
+        printf("No Profit No Loss\n");
+    }
+
+    return 0;
+}
---
+++ day14Q27.c
@@ -0,0 +1,17 @@
+//Q27: Write a program to print the sum of the first n odd numbers.
+
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for (int i = 1, count = 0; count < n; i += 2, count++) {
+        sum += i;
+    }
+
+    printf("Sum = %d\n", sum);
+    return 0;
+}
